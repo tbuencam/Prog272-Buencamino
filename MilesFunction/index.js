247c6eafcf1db4308388fb037fefb63b2f344524
tbuencam <tbuencam@gmail.com>
2014-01-18 13:31:40 -0800

    Adding Project 02 (Miles Function) to my PROG 272 repository.

---
+++ MilesFunction/index.js
@@ -0,0 +1,13 @@
+/**
+ * @author Terence
+ */
+/* jshint strict:true */
+
+
+function milesConvert(miles) {'use strict';
+	return miles * 5280;
+}
+
+/* Function invocation and output to console log:*/
+console.log('3 miles is this many feet: ' + milesConvert(3));
+
