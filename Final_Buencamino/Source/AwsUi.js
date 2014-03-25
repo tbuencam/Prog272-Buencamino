d59b425a09b9455c25ee166d94c86daf66675bc2
VirtualBoxImages.com <tbuencam@gmail.com>
2014-03-25 04:27:44 -0400

    Adding initial version of the final.

---
+++ Final_Buencamino/Source/AwsUi.js
@@ -0,0 +1,189 @@
+define(['jquery'], function() {'use strict';
+
+    var buttons = null;
+    var options = null;
+    var transformOptions = null;
+    var dataIndex = 0;
+    var dataIndexTransform = 0;
+
+	// Constructor.
+    function AwsUi() {
+        $("#listBuckets").click(listBuckets);
+        $("#copyToS3").click(copyToS3);
+        $("#getOptions").click(getOptions);
+        $("#transformForwardButton").click(forwardTransform);
+        $("#tranformBackButton").click(backwardTransform);
+        $("#forwardButton").click(forward);
+        $("#backButton").click(backward);
+        $("#buildAll").click(buildAll);
+        $("#updateStaging").click(updateStaging);
+        $("#updatePublishing").click(updatePublishing);
+
+        console.log("In AwsUi constructor, calling getBuildConfig.");
+        getBuildConfig();
+        console.log("In AwsUi constructor, calling getOptions.");
+        getOptions();
+    }
+
+    var buildAll = function() {
+        $.getJSON("/buildAll", {
+            options : JSON.stringify(transformOptions),
+            index : dataIndexTransform
+        }, function(result) {
+            $("#buildData").empty();
+            var fileArray = result.data.split("\n");
+            for (var i = 0; i < fileArray.length; i++) {
+                if (fileArray[i].length > 0) {
+                    $("#buildData").append("<li>" + fileArray[i] + "</li>");
+                }
+            }
+        });
+    }; // End buildAll
+ 
+	// Reads staging configuration data from the UI fields, packages it, and sends it to app.js for processing.
+   var updateStaging = function() {
+	   var pathToPython = $("#pathToPython").val();
+	   var copyFrom = $("#copyFrom").val();	   
+	   var copyTo = $("#copyTo").val();
+	   // Make filesToCopy into an array.
+	   var filesToCopy = [];
+	   filesToCopy = $("#filesToCopy").val().split(',');
+	   	   	   
+        $.getJSON("/updateStaging", {
+            options : JSON.stringify(transformOptions),
+            index : dataIndexTransform,
+            newPathToPython : pathToPython,
+            newCopyFrom : copyFrom,
+            newCopyTo : copyTo,
+            newFilesToCopy : filesToCopy,
+        }, function(result) {
+			// Refreshing the section from the database by calling getBuildConfig.
+			getBuildConfig();            
+        });
+    }; // End updateStaging
+    
+	// Reads publishing configuration data from the UI fields, packages it, and sends it to app.js for processing.
+   var updatePublishing = function() {
+	   var pathToConfig = $("#pathToConfig").val();
+	   var reallyWrite = $("#reallyWrite").val();  
+	   var bucketName = $("#bucketName").val();	   
+	   var folderToWalk = $("#folderToWalk").val();	   
+	   var s3RootFolder = $("#s3RootFolder").val();	    
+	   var createFolderToWalkOnS3 = $("#createFolderToWalkOnS3").val();	   
+	   var createIndex = $("#createIndex").val();	   	   
+	   // Make filesToIgnore into an array.
+	   var filesToIgnore = [];
+	   filesToIgnore = $("#filesToIgnore").val().split(',');
+	   	   	   
+        $.getJSON("/updatePublishing", {
+            options : JSON.stringify(options),
+            index : dataIndex,            
+            newPathToConfig : pathToConfig,
+            newReallyWrite : reallyWrite,
+            newBucketName : bucketName,
+            newFolderToWalk : folderToWalk,
+            newS3RootFolder : s3RootFolder,
+            newCreateFolderToWalkOnS3 : createFolderToWalkOnS3,
+            newCreateIndex : createIndex,
+            newFilesToIgnore : filesToIgnore,       
+        }, function(result) {
+			// Refreshing the section from the database by calling getOptions.
+			getOptions();       
+        });
+    }; // End updatePublishing
+       
+
+    var copyToS3 = function() {
+        $.getJSON("/copyToS3", {
+            options : JSON.stringify(options[dataIndex])
+        }, function(data) {
+            $("#copyResult").html("Result: " + data.result);
+        });
+    };
+
+	// Displays the build/staging (Transform Configuration) information in the first section. 
+    var displayTransformConfig = function(options) {
+        $("#pathToPython").val(options.pathToPython);
+        $("#copyFrom").val(options.copyFrom);        
+        $("#copyTo").val(options.copyTo);       
+        $("#filesToCopy").val(options.filesToCopy);        
+    }; // End displayTransformConfig
+
+	// Displays the publishing (Copy to S3) information in the second section.
+    var displayOptions = function(options) {
+        $("#currentDocument").html(dataIndex + 1); // This is just a <p>.
+        $("#pathToConfig").val(options.pathToConfig);
+        $("#reallyWrite").val(options.reallyWrite ? "true" : "false");
+        $("#bucketName").val(options.bucketName);
+        $("#folderToWalk").val(options.folderToWalk);
+        $("#s3RootFolder").val(options.s3RootFolder);
+        $("#createFolderToWalkOnS3").val(options.createFolderToWalkOnS3 ? "true" : "false");
+        $("#createIndex").val(options.createIndex ? "true" : "false");
+        $("#filesToIgnore").val(options.filesToIgnore);
+    };
+
+	// Fetches the data and calls displayTransformConfig to populate the first section.
+    var getBuildConfig = function() {
+        $.getJSON("/getBuildConfig", function(optionsInit) {
+            transformOptions = optionsInit;
+            displayTransformConfig(transformOptions[dataIndexTransform]);
+        }).fail(function(a) {
+            alert(JSON.stringify(a));
+        });
+    };
+    
+ 	// Fetches the data and calls displayOptions to populate the second section.   
+    var getOptions = function() {
+        $.getJSON("/getOptions", function(optionsInit) {
+            options = optionsInit;
+            $('#documentCount').html(options.length);
+            displayOptions(options[0]);
+        }).fail(function(a) {
+            alert(JSON.stringify(a));
+        });
+    };
+
+    var forwardTransform = function() {
+        if (dataIndexTransform < transformOptions.length - 1) {
+            dataIndexTransform++;
+            displayTransformConfig(transformOptions[dataIndexTransform]);
+        }
+    };
+
+    var backwardTransform = function() {
+        if (dataIndexTransform > 0) {
+            dataIndexTransform--;
+            displayTransformConfig(transformOptions[dataIndexTransform]);
+            return dataIndexTransform;
+        }
+        return dataIndexTransform;
+    };
+
+    var forward = function() {
+        if (dataIndex < options.length - 1) {
+            dataIndex++;
+            displayOptions(options[dataIndex]);
+        }
+    };
+
+    var backward = function() {
+        if (dataIndex > 0) {
+            dataIndex--;
+            displayOptions(options[dataIndex]);
+            return true;
+        }
+        return false;
+    };
+
+    var listBuckets = function() {
+        $.getJSON("/listBuckets", {
+            options : JSON.stringify(options[dataIndex])
+        }, function(data) {
+            for (var i = 0; i < data.length; i++) {
+                $("#buckets").append("<li>" + data[i] + "</li>");
+            }
+        });
+    };
+
+    return AwsUi;
+});
---
+++ Final_Buencamino/app.js
@@ -0,0 +1,141 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+//var routes = require('./routes');
+//var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var walkDirs = require("./Source/WalkDirs").walkDirs;
+var s3Code = require("./Source/S3Code");
+var fs = require("fs");
+var exec = require('child_process').exec;
+// Adding to read from Mongo
+var qm = require('./Source/QueryMongo');
+var queryMongo = qm.QueryMongo;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 30025);
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Source')));
+app.use(express.static(path.join(__dirname, 'Images')));
+app.use(express.favicon('Images/favicon16.ico'));
+// app.use(express.favicon(path.join(__dirname, 'favicon16.ico')));
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+app.get('/', function(request, response) { 'use strict';
+    var html = fs.readFileSync(__dirname + '/public/index.html');
+    response.writeHeader(200, {"Content-Type": "text/html"});   
+    response.write(html);
+    response.end();
+});
+
+//app.get('/', routes.index);
+// app.get('/users', user.list);
+
+/*
+ * You will need to edit one or more objects in Options.json. 
+ * They have this general format
+
+var options = {
+		pathToConfig: '/home/charlie/config.json',		
+		reallyWrite: true, 
+		bucketName: 'bucket01.elvenware.com',
+		folderToWalk: "Files",
+		s3RootFolder: "FilesTwo",
+		createFolderToWalkOnS3: true,
+		createIndex: true,
+		filesToIgnore: ['Thumbs.db', '.gitignore', 'MyFile.html']
+};
+ 
+ * Before filling it out, see the README file for this project. 
+ */	
+
+// Reads from Mongo database instead of the file system.
+app.get('/getOptions', function(request, response) {'use strict';
+	console.log("In app.js, getOptions Express called");
+	queryMongo.getCollection(response, 'CopytoAwsOptions');  // Passing in collection name.
+	//var options = fs.readFileSync("Options.json", 'utf8');
+	//options = JSON.parse(options);
+	//response.send(options);
+});
+
+app.get('/listBuckets', function(request, response) {'use strict';
+    console.log("ListBuckets called");
+    console.log(request.query);
+	var options = JSON.parse(request.query.options);
+	console.log("ListBuckets: ", options.pathToConfig);
+	s3Code.loadConfig(options.pathToConfig);
+	s3Code.listBuckets(response, true);
+});
+
+app.get('/copyToS3', function(request, response) {'use strict';
+	console.log(typeof request.query.options);	
+	var options = JSON.parse(request.query.options);
+	console.log(options);
+	walkDirs(options, response);
+});
+
+var buildAll = function(response, config, index) { 'use strict';
+	console.log("BuildAll was called");
+	// var config = fs.readFileSync("MarkdownTransformConfig.json", 'utf8');	
+	// config = JSON.parse(config);
+	var command = config[index].pathToPython + " MarkdownTransform.py -i " + index;	
+	try {
+		exec(command, function callback(error, stdout, stderr) {
+			// Read in the HTML send the HTML to the client
+			console.log("convertToHtml was called er: ", error);
+			console.log("convertToHtml was called so: ", stdout);
+			console.log("convertToHtml was called se: ", stderr);
+			response.send({ "result": "Success", "data": stdout });
+		});
+	} catch(e) {
+		console.log(e.message);
+		response.send( { "result" : "error", "data": e });
+	}
+};
+
+app.get('/buildAll', function(request, response) { 'use strict';
+	console.log("buildAll called");	
+	var options = JSON.parse(request.query.options);
+	buildAll(response, options, request.query.index);
+});
+
+app.get('/updateStaging', function(request, response) { 'use strict';
+	console.log("updateStaging called");
+	// Call the QueryMongo with the request to do the update.
+	console.log("Calling queryMongo.updateTCOCollection"); // Pass in the request because it has all the info needed.
+	queryMongo.updateTCOCollection(request, response);  
+});
+
+app.get('/updatePublishing', function(request, response) { 'use strict';
+	console.log("updatePublishing called");
+	// Call the QueryMongo with the request to do the update.
+	console.log("Calling queryMongo.updateCTACollection"); // Pass in the request because it has all the info needed.
+	queryMongo.updateCTACollection(request, response);
+});
+
+// Reworked to read from Mongo database. Call getCollection with parameter for collection.
+app.get('/getBuildConfig', function(request, response) { 'use strict';
+	console.log('In app.js, getBuildConfig called');
+	queryMongo.getCollection(response, 'TransformCopyOptions');  // Passing in collection name.
+	// console.log("Data received: "+ response);
+});
+
+http.createServer(app).listen(app.get('port'), function() {'use strict';
+	console.log('Express server listening on port ' + app.get('port'));
+});
